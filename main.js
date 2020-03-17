3989a312a7c30892d6161901448292d33e3c6ab8
Sejin Kim <kim3@kenyon.edu>
2020-03-17 10:50:56 -0400

    :arrow_up: :art: Added semicolons to JS. Added electron-cgi dep.

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, Menu } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, Menu } = require('electron');
+const { ConnectionBuilder } = require('electron-cgi');
+const path = require('path');
 
 // Force a platform dependency on Darwin if the operating system is macOS because thanks Apple
-const isMac = process.platform === 'darwin'
+const isMac = process.platform === 'darwin';
 
 // Create a menu template
 // Ripped straight from https://www.electronjs.org/docs/api/menu
@@ -105,8 +106,8 @@ const template = [
 ]
 
 // Call and display the menu from the above provided menu template
-const menu = Menu.buildFromTemplate(template)
-Menu.setApplicationMenu(menu)
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
 
 function createWindow() {
     // Create the browser window.
@@ -119,7 +120,7 @@ function createWindow() {
     })
 
     // and load the index.html of the app.
-    mainWindow.loadFile('index.html')
+    mainWindow.loadFile('index.html');
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
@@ -128,19 +129,19 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit();
 })
 
 app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
 })
 
 // In this file you can include the rest of your app's specific main process
